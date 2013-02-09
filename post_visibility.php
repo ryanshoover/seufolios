81219566198e7c4d3aefb32242928c1f4e3922bd
Ryan Hoover <ryanshoover@gmail.com>
2013-02-09 11:01:23 -0600

    clean up pointless comments

--- post_visibility.php
+++ post_visibility.php
@@ -31,6 +31,7 @@ add_filter('wp_nav_menu_args', 'seu_edit_nav_menu', 10000);
 
 //add_filter('getarchives_where',	array('social_access_control','filter_getarchives_where'), 10000);
 
+//--------------------------------------------
 //next two add filter to wp_nav_menu -> new function for custom nav menus
 function seu_edit_nav_menu($args = '') {
 	$args['walker'] = new SEU_Walker_Nav_Menu;
@@ -38,55 +39,16 @@ function seu_edit_nav_menu($args = '') {
 }
 
 class SEU_Walker_Nav_Menu extends Walker {
-	/**
-	 * @see Walker::$tree_type
-	 * @since 3.0.0
-	 * @var string
-	 */
 	var $tree_type = array( 'post_type', 'taxonomy', 'custom' );
-
-	/**
-	 * @see Walker::$db_fields
-	 * @since 3.0.0
-	 * @todo Decouple this.
-	 * @var array
-	 */
 	var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
-
-	/**
-	 * @see Walker::start_lvl()
-	 * @since 3.0.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param int $depth Depth of page. Used for padding.
-	 */
 	function start_lvl( &$output, $depth = 0, $args = array() ) {
 		$indent = str_repeat("\t", $depth);
 		$output .= "\n$indent<ul class=\"sub-menu\">\n";
 	}
-
-	/**
-	 * @see Walker::end_lvl()
-	 * @since 3.0.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param int $depth Depth of page. Used for padding.
-	 */
 	function end_lvl( &$output, $depth = 0, $args = array() ) {
 		$indent = str_repeat("\t", $depth);
 		$output .= "$indent</ul>\n";
 	}
-
-	/**
-	 * @see Walker::start_el()
-	 * @since 3.0.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param object $item Menu item data object.
-	 * @param int $depth Depth of menu item. Used for padding.
-	 * @param int $current_page Menu item ID.
-	 * @param object $args
-	 */
 	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 		//added by RSH
 		if (post_should_be_hidden($item->object_id)) return false;
@@ -119,15 +81,6 @@ class SEU_Walker_Nav_Menu extends Walker {
 
 		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
-
-	/**
-	 * @see Walker::end_el()
-	 * @since 3.0.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param object $item Page data object. Not used.
-	 * @param int $depth Depth of page. Not Used.
-	 */
 	function end_el( &$output, $item, $depth = 0, $args = array() ) {
 		//added by RSH
 		if (post_should_be_hidden($item->object_id)) return false;
