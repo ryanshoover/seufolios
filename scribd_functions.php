de1052f75769aeff40d758fc15a03b406cde111b
Ryan Hoover <ryanshoover@gmail.com>
2013-05-08 15:58:49 -0500

    forgot one...

--- scribd_functions.php
+++ scribd_functions.php
@@ -6,7 +6,6 @@ add_option('scribd_api_key', 'cpkion25nntjfxqiom2u');
 add_option('scribd_secret', 'sec-bdw42wqvtbfk6gjrq39cibgw47');
 add_option('scribd_publisher_id', 'pub-9427655662973965355');
 
-<<<<<<< HEAD
 //enable end user options
 function enable_scribd_functions() {
 	//Action hooks
@@ -18,17 +17,7 @@ function enable_scribd_functions() {
 	add_action( 'admin_menu' , 'add_scribd_management');
 	add_action('admin_head-media-upload-popup', 'file_upload_instruction');
 }
-=======
-//Action hooks
-add_action('wp_head', 'seufolios_headerinfo');
-add_filter('media_upload_tabs', 'seu_scribd_media_menu');
-add_action('media_upload_scribd', 'scribd_media_menu_handle');
-add_action('init', 'clear_shortcode'); //used to clear the Scribd shortcode used by Jetpack pluging
-//add_shortcode( 'scribd', 'scribd_shortcode' );
-add_action( 'admin_menu' , 'add_scribd_management');
-add_action('admin_head-media-upload-popup', 'file_upload_instruction');
 
->>>>>>> 25783114dcb06e099e156728e628dcd854c8a37a
 //*****
 //Add Scribd media type
 
